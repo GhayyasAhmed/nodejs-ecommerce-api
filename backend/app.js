d09efd17f0d5f794ec1b89d7bbd835e8c5ffffcc
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-04 06:17:09 +0500

    feat: search, filter, and pagination

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const errorMiddleware = require("./middlewares/error")
 
 app.use(express.json())
 
+// Revert to Express 4 behavior for nested queries
+app.set('query parser', 'extended'); 
+
 
 const products = require("./routes/productsRoute")
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErros")
-
+const ApiFeatures = require("../utils/apifeatures")
 
 // admin route
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,9 +13,12 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 })
 
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-    const products = await Product.find({})
+    const resultPerPage = 5
+    const productCount = await Product.countDocuments();
+    const apiFeature = new ApiFeatures(Product.find({}), req.query).search().filter().pagination(resultPerPage)
+    const products = await apiFeature.query
 
-    res.status(200).json({ message: "Products fetched successfully", data: products })
+    res.status(200).json({ success: true, count: productCount, data: products })
 })
 
 
