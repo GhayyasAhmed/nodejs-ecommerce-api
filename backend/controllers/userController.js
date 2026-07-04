1833f8b7a18778997682fb7a0a3d21360ef9e0bd
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-04 18:27:02 +0500

    feat: logout route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,3 +42,9 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res)
 
 })
+
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+    res.cookie("token", null, { expires: new Date(Date.now()), httpOnly: true })
+
+    res.status(200).json({ success: true, message: "logged out" })
+})
